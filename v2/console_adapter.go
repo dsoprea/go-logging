95a348eb79daf03f779bae75b69ea699c668f82e
Dustin Oprea <dustin@randomingenuity.com>
2020-07-10 14:45:13 -0400

    v2: Report-card fixes

--- v2/console_adapter.go
+++ v2/console_adapter.go
@@ -1,37 +1,42 @@
 package log
 
 import (
-    golog "log"
+	golog "log"
 )
 
+// ConsoleLogAdapter prints logging to STDOUT.
 type ConsoleLogAdapter struct {
-
 }
 
+// NewConsoleLogAdapter returns a new ConsoleLogAdapter.
 func NewConsoleLogAdapter() LogAdapter {
-    return new(ConsoleLogAdapter)
+	return new(ConsoleLogAdapter)
 }
 
+// Debugf logs a debugging message.
 func (cla *ConsoleLogAdapter) Debugf(lc *LogContext, message *string) error {
-    golog.Println(*message)
+	golog.Println(*message)
 
-    return nil
+	return nil
 }
 
+// Infof logs an info message.
 func (cla *ConsoleLogAdapter) Infof(lc *LogContext, message *string) error {
-    golog.Println(*message)
+	golog.Println(*message)
 
-    return nil
+	return nil
 }
 
+// Warningf logs a warning message.
 func (cla *ConsoleLogAdapter) Warningf(lc *LogContext, message *string) error {
-    golog.Println(*message)
+	golog.Println(*message)
 
-    return nil
+	return nil
 }
 
+// Errorf logs an error message.
 func (cla *ConsoleLogAdapter) Errorf(lc *LogContext, message *string) error {
-    golog.Println(*message)
+	golog.Println(*message)
 
-    return nil
+	return nil
 }
--- v2/log_test.go
+++ v2/log_test.go
@@ -1,413 +1,413 @@
 package log
 
 import (
-    e "errors"
-    "testing"
+	e "errors"
+	"testing"
 
-    "math/rand"
+	"math/rand"
 )
 
 // Extends the default environment configuration-provider to set the level to
 // "debug" (so none of our messages get filtered).
 type testConfigurationProvider struct {
-    levelName LogLevelName
+	levelName LogLevelName
 }
 
 func newTestConfigurationProvider(levelName LogLevelName) ConfigurationProvider {
-    if levelName == "" {
-        levelName = levelNameError
-    }
+	if levelName == "" {
+		levelName = levelNameError
+	}
 
-    return &testConfigurationProvider{
-        levelName: levelName,
-    }
+	return &testConfigurationProvider{
+		levelName: levelName,
+	}
 }
 
 func (ec *testConfigurationProvider) Format() string {
-    return ""
+	return ""
 }
 
 func (ec *testConfigurationProvider) DefaultAdapterName() string {
-    return ""
+	return ""
 }
 
 func (ec *testConfigurationProvider) LevelName() LogLevelName {
-    return ec.levelName
+	return ec.levelName
 }
 
 func (ec *testConfigurationProvider) IncludeNouns() string {
-    return ""
+	return ""
 }
 
 func (ec *testConfigurationProvider) ExcludeNouns() string {
-    return ""
+	return ""
 }
 
 func (ec *testConfigurationProvider) ExcludeBypassLevelName() LogLevelName {
-    return ""
+	return ""
 }
 
 // A test logging-adapter that sets flags as certain messages are received.
 type testLogAdapter struct {
-    id int
+	id int
 
-    debugTriggered   bool
-    infoTriggered    bool
-    warningTriggered bool
-    errorTriggered   bool
+	debugTriggered   bool
+	infoTriggered    bool
+	warningTriggered bool
+	errorTriggered   bool
 }
 
 func newTestLogAdapter() LogAdapter {
-    return &testLogAdapter{
-        id: rand.Int(),
-    }
+	return &testLogAdapter{
+		id: rand.Int(),
+	}
 }
 
 func (tla *testLogAdapter) Debugf(lc *LogContext, message *string) error {
-    tla.debugTriggered = true
+	tla.debugTriggered = true
 
-    return nil
+	return nil
 }
 
 func (tla *testLogAdapter) Infof(lc *LogContext, message *string) error {
-    tla.infoTriggered = true
+	tla.infoTriggered = true
 
-    return nil
+	return nil
 }
 
 func (tla *testLogAdapter) Warningf(lc *LogContext, message *string) error {
-    tla.warningTriggered = true
+	tla.warningTriggered = true
 
-    return nil
+	return nil
 }
 
 func (tla *testLogAdapter) Errorf(lc *LogContext, message *string) error {
-    tla.errorTriggered = true
+	tla.errorTriggered = true
 
-    return nil
+	return nil
 }
 
 // Tests
 
 func TestConfigurationOverride(t *testing.T) {
-    cs := getConfigState()
-    defer func() {
-        setConfigState(cs)
-    }()
+	cs := getConfigState()
+	defer func() {
+		setConfigState(cs)
+	}()
 
-    levelName = "xyz"
+	levelName = "xyz"
 
-    // Overwrite configuration, first thing.
-    tcp := newTestConfigurationProvider(levelNameDebug)
-    LoadConfiguration(tcp)
+	// Overwrite configuration, first thing.
+	tcp := newTestConfigurationProvider(levelNameDebug)
+	LoadConfiguration(tcp)
 
-    if levelName != levelNameDebug {
-        t.Fatalf("The test configuration-provider didn't override the level properly: [%s]", levelName)
-    }
+	if levelName != levelNameDebug {
+		t.Fatalf("The test configuration-provider didn't override the level properly: [%s]", levelName)
+	}
 }
 
 func TestConfigurationLevelDirectOverride(t *testing.T) {
-    // Overwrite configuration, first thing.
-    tcp := newTestConfigurationProvider("")
-    LoadConfiguration(tcp)
+	// Overwrite configuration, first thing.
+	tcp := newTestConfigurationProvider("")
+	LoadConfiguration(tcp)
 
-    ClearAdapters()
+	ClearAdapters()
 
-    tla1 := newTestLogAdapter()
-    AddAdapter("test", tla1)
+	tla1 := newTestLogAdapter()
+	AddAdapter("test", tla1)
 
-    l := NewLoggerWithAdapterName("logTest", "test")
+	l := NewLoggerWithAdapterName("logTest", "test")
 
-    // Usually we don't configure until the first message. Force it.
-    l.doConfigure(false)
-    tla2 := l.Adapter().(*testLogAdapter)
+	// Usually we don't configure until the first message. Force it.
+	l.doConfigure(false)
+	tla2 := l.Adapter().(*testLogAdapter)
 
-    if tla2.debugTriggered != false {
-        t.Fatalf("Debug flag should've been FALSE initially but wasn't.")
-    }
+	if tla2.debugTriggered != false {
+		t.Fatalf("Debug flag should've been FALSE initially but wasn't.")
+	}
 
-    // Set the level high to prevent logging, first.
-    levelName = levelNameError
+	// Set the level high to prevent logging, first.
+	levelName = levelNameError
 
-    // Force a reconfig (which will bring in the new level).
-    l.doConfigure(true)
+	// Force a reconfig (which will bring in the new level).
+	l.doConfigure(true)
 
-    // Re-retrieve. This is reconstructed during reconfiguration.
-    tla3 := l.Adapter().(*testLogAdapter)
+	// Re-retrieve. This is reconstructed during reconfiguration.
+	tla3 := l.Adapter().(*testLogAdapter)
 
-    l.Debugf(nil, "Debug message")
+	l.Debugf(nil, "Debug message")
 
-    if tla3.debugTriggered != false {
-        t.Fatalf("Debug message not through but wasn't supposed to.")
-    }
+	if tla3.debugTriggered != false {
+		t.Fatalf("Debug message not through but wasn't supposed to.")
+	}
 
-    // Now, set the level low to allow logging.
-    levelName = levelNameDebug
+	// Now, set the level low to allow logging.
+	levelName = levelNameDebug
 
-    // Force a reconfig (which will bring in the new level).
-    l.doConfigure(true)
+	// Force a reconfig (which will bring in the new level).
+	l.doConfigure(true)
 
-    // Re-retrieve. This is reconstructed during reconfiguration.
-    tla4 := l.Adapter().(*testLogAdapter)
+	// Re-retrieve. This is reconstructed during reconfiguration.
+	tla4 := l.Adapter().(*testLogAdapter)
 
-    l.Debugf(nil, "Debug message")
+	l.Debugf(nil, "Debug message")
 
-    if tla4.debugTriggered == false {
-        t.Fatalf("Debug message not getting through.")
-    }
+	if tla4.debugTriggered == false {
+		t.Fatalf("Debug message not getting through.")
+	}
 }
 
 func TestConfigurationLevelProviderOverride(t *testing.T) {
-    cs := getConfigState()
-    defer func() {
-        setConfigState(cs)
-    }()
+	cs := getConfigState()
+	defer func() {
+		setConfigState(cs)
+	}()
 
-    // Overwrite configuration, first thing.
-    tcp := newTestConfigurationProvider("")
-    LoadConfiguration(tcp)
+	// Overwrite configuration, first thing.
+	tcp := newTestConfigurationProvider("")
+	LoadConfiguration(tcp)
 
-    ClearAdapters()
+	ClearAdapters()
 
-    tla1 := newTestLogAdapter()
-    AddAdapter("test", tla1)
+	tla1 := newTestLogAdapter()
+	AddAdapter("test", tla1)
 
-    l := NewLoggerWithAdapterName("logTest", "test")
+	l := NewLoggerWithAdapterName("logTest", "test")
 
-    // Usually we don't configure until the first message. Force it.
-    l.doConfigure(false)
-    tla2 := l.Adapter().(*testLogAdapter)
+	// Usually we don't configure until the first message. Force it.
+	l.doConfigure(false)
+	tla2 := l.Adapter().(*testLogAdapter)
 
-    if tla2.debugTriggered != false {
-        t.Fatalf("Debug flag should've been FALSE initially but wasn't.")
-    }
+	if tla2.debugTriggered != false {
+		t.Fatalf("Debug flag should've been FALSE initially but wasn't.")
+	}
 
-    // Set the level high to prevent logging, first.
-    tcp = newTestConfigurationProvider(levelNameError)
-    LoadConfiguration(tcp)
+	// Set the level high to prevent logging, first.
+	tcp = newTestConfigurationProvider(levelNameError)
+	LoadConfiguration(tcp)
 
-    // Force a reconfig (which will bring in the new level).
-    l.doConfigure(true)
+	// Force a reconfig (which will bring in the new level).
+	l.doConfigure(true)
 
-    // Re-retrieve. This is reconstructed during reconfiguration.
-    tla3 := l.Adapter().(*testLogAdapter)
+	// Re-retrieve. This is reconstructed during reconfiguration.
+	tla3 := l.Adapter().(*testLogAdapter)
 
-    l.Debugf(nil, "Debug message")
+	l.Debugf(nil, "Debug message")
 
-    if tla3.debugTriggered != false {
-        t.Fatalf("Debug message not through but wasn't supposed to.")
-    }
+	if tla3.debugTriggered != false {
+		t.Fatalf("Debug message not through but wasn't supposed to.")
+	}
 
-    // Now, set the level low to allow logging.
-    tcp = newTestConfigurationProvider(levelNameDebug)
-    LoadConfiguration(tcp)
+	// Now, set the level low to allow logging.
+	tcp = newTestConfigurationProvider(levelNameDebug)
+	LoadConfiguration(tcp)
 
-    // Force a reconfig (which will bring in the new level).
-    l.doConfigure(true)
+	// Force a reconfig (which will bring in the new level).
+	l.doConfigure(true)
 
-    // Re-retrieve. This is reconstructed during reconfiguration.
-    tla4 := l.Adapter().(*testLogAdapter)
+	// Re-retrieve. This is reconstructed during reconfiguration.
+	tla4 := l.Adapter().(*testLogAdapter)
 
-    l.Debugf(nil, "Debug message")
+	l.Debugf(nil, "Debug message")
 
-    if tla4.debugTriggered == false {
-        t.Fatalf("Debug message not getting through.")
-    }
+	if tla4.debugTriggered == false {
+		t.Fatalf("Debug message not getting through.")
+	}
 }
 
 func TestDefaultAdapterAssignment(t *testing.T) {
-    SetDefaultAdapterName("")
+	SetDefaultAdapterName("")
 
-    ClearAdapters()
+	ClearAdapters()
 
-    tla := newTestLogAdapter()
-    AddAdapter("test1", tla)
+	tla := newTestLogAdapter()
+	AddAdapter("test1", tla)
 
-    an := GetDefaultAdapterName()
-    if an == "" {
-        t.Fatalf("Default adapter not set after registration.")
-    }
+	an := GetDefaultAdapterName()
+	if an == "" {
+		t.Fatalf("Default adapter not set after registration.")
+	}
 
-    an = GetDefaultAdapterName()
-    if an != "test1" {
-        t.Fatalf("Default adapter not set to our adapter after registration: [%v]", an)
-    }
+	an = GetDefaultAdapterName()
+	if an != "test1" {
+		t.Fatalf("Default adapter not set to our adapter after registration: [%v]", an)
+	}
 
-    SetDefaultAdapterName("test2")
-    an = GetDefaultAdapterName()
-    if an != "test2" {
-        t.Fatalf("SetDefaultAdapterName() did not set default adapter correctly: [%v]", an)
-    }
+	SetDefaultAdapterName("test2")
+	an = GetDefaultAdapterName()
+	if an != "test2" {
+		t.Fatalf("SetDefaultAdapterName() did not set default adapter correctly: [%v]", an)
+	}
 }
 
 func TestAdapter(t *testing.T) {
-    cs := getConfigState()
-    defer func() {
-        setConfigState(cs)
-    }()
+	cs := getConfigState()
+	defer func() {
+		setConfigState(cs)
+	}()
 
-    // Overwrite configuration, first thing.
-    tcp := newTestConfigurationProvider(levelNameDebug)
-    LoadConfiguration(tcp)
+	// Overwrite configuration, first thing.
+	tcp := newTestConfigurationProvider(levelNameDebug)
+	LoadConfiguration(tcp)
 
-    ClearAdapters()
+	ClearAdapters()
 
-    tla1 := newTestLogAdapter()
-    AddAdapter("test", tla1)
+	tla1 := newTestLogAdapter()
+	AddAdapter("test", tla1)
 
-    l := NewLoggerWithAdapterName("logTest", "test")
+	l := NewLoggerWithAdapterName("logTest", "test")
 
-    l.doConfigure(false)
+	l.doConfigure(false)
 
-    tla2 := l.Adapter().(*testLogAdapter)
+	tla2 := l.Adapter().(*testLogAdapter)
 
-    l.Debugf(nil, "Debug message")
-    if tla2.debugTriggered == false {
-        t.Fatalf("Debug message not getting through.")
-    }
+	l.Debugf(nil, "Debug message")
+	if tla2.debugTriggered == false {
+		t.Fatalf("Debug message not getting through.")
+	}
 
-    l.Infof(nil, "Info message")
-    if tla2.infoTriggered == false {
-        t.Fatalf("Info message not getting through.")
-    }
+	l.Infof(nil, "Info message")
+	if tla2.infoTriggered == false {
+		t.Fatalf("Info message not getting through.")
+	}
 
-    l.Warningf(nil, "Warning message")
-    if tla2.warningTriggered == false {
-        t.Fatalf("Warning message not getting through.")
-    }
+	l.Warningf(nil, "Warning message")
+	if tla2.warningTriggered == false {
+		t.Fatalf("Warning message not getting through.")
+	}
 
-    err := e.New("an error happened")
-    l.Errorf(nil, err, "Error message")
-    if tla2.errorTriggered == false {
-        t.Fatalf("Error message not getting through.")
-    }
+	err := e.New("an error happened")
+	l.Errorf(nil, err, "Error message")
+	if tla2.errorTriggered == false {
+		t.Fatalf("Error message not getting through.")
+	}
 }
 
 func TestStaticConfiguration(t *testing.T) {
-    scp := NewStaticConfigurationProvider()
+	scp := NewStaticConfigurationProvider()
 
-    cs := getConfigState()
-    defer func() {
-        setConfigState(cs)
-    }()
+	cs := getConfigState()
+	defer func() {
+		setConfigState(cs)
+	}()
 
-    scp.SetFormat("aa")
-    scp.SetDefaultAdapterName("bb")
-    scp.SetLevelName("cc")
-    scp.SetIncludeNouns("dd")
-    scp.SetExcludeNouns("ee")
-    scp.SetExcludeBypassLevelName("ff")
+	scp.SetFormat("aa")
+	scp.SetDefaultAdapterName("bb")
+	scp.SetLevelName("cc")
+	scp.SetIncludeNouns("dd")
+	scp.SetExcludeNouns("ee")
+	scp.SetExcludeBypassLevelName("ff")
 
-    LoadConfiguration(scp)
+	LoadConfiguration(scp)
 
-    if format != "aa" {
-        t.Fatalf("Static configuration provider was not set correctly: format")
-    }
+	if format != "aa" {
+		t.Fatalf("Static configuration provider was not set correctly: format")
+	}
 
-    if defaultAdapterName != "bb" {
-        t.Fatalf("Static configuration provider was not set correctly: defaultAdapterName")
-    }
+	if defaultAdapterName != "bb" {
+		t.Fatalf("Static configuration provider was not set correctly: defaultAdapterName")
+	}
 
-    if levelName != "cc" {
-        t.Fatalf("Static configuration provider was not set correctly: levelName")
-    }
+	if levelName != "cc" {
+		t.Fatalf("Static configuration provider was not set correctly: levelName")
+	}
 
-    if includeNouns != "dd" {
-        t.Fatalf("Static configuration provider was not set correctly: includeNouns")
-    }
+	if includeNouns != "dd" {
+		t.Fatalf("Static configuration provider was not set correctly: includeNouns")
+	}
 
-    if excludeNouns != "ee" {
-        t.Fatalf("Static configuration provider was not set correctly: excludeNouns")
-    }
+	if excludeNouns != "ee" {
+		t.Fatalf("Static configuration provider was not set correctly: excludeNouns")
+	}
 
-    if excludeBypassLevelName != "ff" {
-        t.Fatalf("Static configuration provider was not set correctly: excludeBypassLevelName")
-    }
+	if excludeBypassLevelName != "ff" {
+		t.Fatalf("Static configuration provider was not set correctly: excludeBypassLevelName")
+	}
 }
 
 func TestNoAdapter(t *testing.T) {
-    ClearAdapters()
+	ClearAdapters()
 
-    l := NewLogger("logTest")
+	l := NewLogger("logTest")
 
-    if l.Adapter() != nil {
-        t.Fatalf("Logger has an adapter at init when no adapters were available.")
-    }
+	if l.Adapter() != nil {
+		t.Fatalf("Logger has an adapter at init when no adapters were available.")
+	}
 
-    l.doConfigure(false)
+	l.doConfigure(false)
 
-    if l.Adapter() != nil {
-        t.Fatalf("Logger has an adapter after configuration no adapters were available.")
-    }
+	if l.Adapter() != nil {
+		t.Fatalf("Logger has an adapter after configuration no adapters were available.")
+	}
 
-    // Should execute, but nothing will happen.
-    err := e.New("an error happened")
-    l.Errorf(nil, err, "Error message")
+	// Should execute, but nothing will happen.
+	err := e.New("an error happened")
+	l.Errorf(nil, err, "Error message")
 }
 
 func TestNewLogger(t *testing.T) {
-    noun := "logTest"
+	noun := "logTest"
 
-    l := NewLogger(noun)
-    if l.noun != noun {
-        t.Fatalf("Noun not correct: [%s]", l.noun)
-    }
+	l := NewLogger(noun)
+	if l.noun != noun {
+		t.Fatalf("Noun not correct: [%s]", l.noun)
+	}
 }
 
 func TestNewLoggerWithAdapterName(t *testing.T) {
-    noun := "logTest"
+	noun := "logTest"
 
-    originalDefaultAdapterName := GetDefaultAdapterName()
+	originalDefaultAdapterName := GetDefaultAdapterName()
 
-    adapterName := "abcdef"
+	adapterName := "abcdef"
 
-    cla := NewConsoleLogAdapter()
-    AddAdapter(adapterName, cla)
+	cla := NewConsoleLogAdapter()
+	AddAdapter(adapterName, cla)
 
-    SetDefaultAdapterName(adapterName)
+	SetDefaultAdapterName(adapterName)
 
-    defer func() {
-        SetDefaultAdapterName(originalDefaultAdapterName)
-        delete(adapters, adapterName)
-    }()
+	defer func() {
+		SetDefaultAdapterName(originalDefaultAdapterName)
+		delete(adapters, adapterName)
+	}()
 
-    l := NewLoggerWithAdapterName(noun, adapterName)
-    if l.noun != noun {
-        t.Fatalf("Noun not correct: [%s]", l.noun)
-    } else if l.an != adapterName {
-        t.Fatalf("Adapter-name not correct: [%s]", l.an)
-    }
+	l := NewLoggerWithAdapterName(noun, adapterName)
+	if l.noun != noun {
+		t.Fatalf("Noun not correct: [%s]", l.noun)
+	} else if l.an != adapterName {
+		t.Fatalf("Adapter-name not correct: [%s]", l.an)
+	}
 }
 
 func TestIs__unwrapped__hit(t *testing.T) {
-    e1 := e.New("test error")
-    if Is(e1, e1) != true {
-        t.Fatalf("Is() should be true for an unwrapped success")
-    }
+	e1 := e.New("test error")
+	if Is(e1, e1) != true {
+		t.Fatalf("Is() should be true for an unwrapped success")
+	}
 }
 
 func TestIs__unwrapped__miss(t *testing.T) {
-    e1 := e.New("test error")
-    e2 := e.New("test error 2")
+	e1 := e.New("test error")
+	e2 := e.New("test error 2")
 
-    if Is(e1, e2) != false {
-        t.Fatalf("Is() should be false for an unwrapped failure")
-    }
+	if Is(e1, e2) != false {
+		t.Fatalf("Is() should be false for an unwrapped failure")
+	}
 }
 
 func TestIs__wrapped__hit(t *testing.T) {
-    e2 := e.New("test error")
-    e1 := Wrap(e2)
-    if Is(e1, e2) != true {
-        t.Fatalf("Is() should be true for a wrapped success")
-    }
+	e2 := e.New("test error")
+	e1 := Wrap(e2)
+	if Is(e1, e2) != true {
+		t.Fatalf("Is() should be true for a wrapped success")
+	}
 }
 
 func TestIs__wrapped__miss(t *testing.T) {
-    e1 := Errorf("test error")
-    e2 := e.New("test error 2")
+	e1 := Errorf("test error")
+	e2 := e.New("test error 2")
 
-    if Is(e1, e2) != false {
-        t.Fatalf("Is() should be false for a wrapped failure")
-    }
+	if Is(e1, e2) != false {
+		t.Fatalf("Is() should be false for a wrapped failure")
+	}
 }
